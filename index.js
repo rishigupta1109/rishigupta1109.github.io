16dadc15b7a10eabe95fb1864e1ab33546478470
Rishi Gupta <grishi634@gmail.com>
2021-07-15 20:47:44 +0530

    contactme

--- index.js
+++ index.js
@@ -3,6 +3,43 @@ let About=document.getElementById("About");
 let Skills=document.getElementById("Skills");
 let innerbar=document.getElementsByClassName("innerbar");
 let dash =document.getElementsByClassName("dash")[0];
+let inputhead =document.getElementsByClassName("inputhead");
+let Name=document.getElementById("Name")
+let Email=document.getElementById("Email")
+let Message=document.getElementById("Message")
+
+inputhead[0].addEventListener("click",()=>{
+    Name.focus();
+})
+inputhead[1].addEventListener("click",()=>{
+    Email.focus();
+})
+inputhead[2].addEventListener("click",()=>{
+    Message.focus();
+})
+
+Name.addEventListener("focus",()=>{
+    inputhead[0].style.top="-100px";
+})
+Name.addEventListener("blur",()=>{
+    if(Name.value.trim().length===0){inputhead[0].style.top="-55px";}
+    
+})
+Email.addEventListener("focus",()=>{
+    inputhead[1].style.top="-100px";
+})
+Email.addEventListener("blur",()=>{
+    if(Name.value.trim().length===0){inputhead[1].style.top="-55px";}
+    
+})
+Message.addEventListener("focus",()=>{
+    inputhead[2].style.top="-100px";
+})
+Message.addEventListener("blur",()=>{
+    if(Name.value.trim().length===0){inputhead[2].style.top="-55px";}
+    
+})
+
 
 About.addEventListener("click",()=>{window.scrollTo(0,0);  dash.style.left= "265px";dash.style.width= "50px";});
 Projects.addEventListener("click",()=>{window.scrollTo(0,1438);dash.style.left= "540px";dash.style.width= "65px";});
@@ -38,5 +75,6 @@ if(scrollY<2500&&scrollY>=1431){
     dash.style.left= "540px";
     dash.style.width= "65px";
 }
+
 },1000)
 
